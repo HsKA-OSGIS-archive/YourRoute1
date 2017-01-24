cb57bfa97540c7a7b8c8d20cb48b86413e406901
BFutterer <bene_reichenau@yahoo.de>
2017-01-24 18:34:20 +0100

    Add JS-file

---
+++ Website/JS/ownWebsite.js
@@ -0,0 +1,13 @@
+function changeRoutertoBRouter() {
+	var router1 = document.querySelector("#iFrameBRouter");
+		router1.style.display = "block";	
+	var router2 = document.querySelector("#iFrameORS");
+		router2.style.display = "none";
+}
+
+function changeRoutertoORS() {
+	var router1 = document.querySelector("#iFrameBRouter");
+		router1.style.display = "none";	
+	var router2 = document.querySelector("#iFrameORS");
+		router2.style.display = "block";
+}
